a3d8d328b1386031fe5df782a52be4b172992e72
shirash1997! <shira.shatz@abra-it.com>
2025-08-24 18:06:57 +0300

    first commit

--- force-app/main/default/lwc/sp3dProductGrid/sp3dProductGrid.js
+++ force-app/main/default/lwc/sp3dProductGrid/sp3dProductGrid.js
@@ -4,5 +4,5 @@ import { loadStyle } from 'lightning/platformResourceLoader';
 export default class Sp3dProductGrid extends LightningElement {
   @api products = [];
   connectedCallback(){ loadStyle(this, styles); }
-  add = (e) => { this.dispatchEvent(new CustomEvent('addtocart', { detail:{ product: e.detail.product } })); }
+  add = (e) => { this.dispatchEvent(new CustomEvent('addtocart', { detail:{ product: e.detail.product } })); } 
 }
